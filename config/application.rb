c7b9ccf4ef27566302bc893de00a9e50546f225b
German Gonzalez <nordicod3@gmail.com>
2017-05-11 23:07:53 -0500

    images fixed

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Test3
+module Postgresql
   class Application < Rails::Application
-    config.serve_static_assets = true
-	config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-	config.assets.compile = true
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-get 'index' => 'home#index'
-get 'band' => 'band#band_view'
-get 'sport' => 'sport#sport_view'
-get 'about_me' => 'about_me#about_view'
+  	get 'index' => 'home#index'
+	get 'band' => 'band#band_view'
+	get 'sport' => 'sport#sport_view'
+	get 'about_me' => 'about_me#about_view'
   root 'home#index'
 end
